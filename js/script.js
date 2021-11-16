022a79d50866247849dccfcb6d0f0814c1a38592
KwagHyeyeong <89637930+nbje1007@users.noreply.github.com>
2021-11-16 16:30:23 +0900

    개인프로필 JS 2차 업로드
    개인프로필 JS 2차 업로드

--- js/script.js
+++ js/script.js
@@ -8,56 +8,39 @@ $(document).ready(function () {
             scrollTop: 0
         });
     });
-    new Swiper('.sw-rede', {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        spaceBetween: 16,
+
+
+    // new Swiper('.sw-rede', {
+    //     slidesPerView: 3,
+    //     slidesPerGroup: 3,
+    //     spaceBetween: 16,
+    //     navigation: {
+    //         nextEl: ".sw-rede-next",
+    //         prevEl: ".sw-rede-prev",
+    //     },
+    //     pagination: {
+    //         el: ".sw-rede-pg",
+    //         type: "fraction",
+    //     },
+    // });
+    // publ slide
+    let sw_publ = new Swiper('.sw-publ', {
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        loop: true,
         navigation: {
-            nextEl: ".sw-rede-next",
-            prevEl: ".sw-rede-prev",
+            nextEl: ".sw-publ-next",
+            prevEl: ".sw-publ-prev",
         },
         pagination: {
-            el: ".sw-rede-pg",
+            el: ".sw-publ-pg",
             type: "fraction",
         },
     });
 
-    var sw_visual = new Swiper('.swiper-container', {
 
-        // 효과 : 필요에 의해서 사용
-        effect: 'fade',
-        fadeEffect: {
-            crossFade: true
-        },
-
-        // // 슬라이드 반복
-        // loop: true,
-
-        // 자동 실행
-        autoplay: {
-            delay: 2000,
-            // 자동실행 유지
-            disableOnInteraction: false,
-        },
-
-        // 모션속도
-        speed: 1000,
-
-        // 좌우이동
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-
-        // 슬라이드 목록
-        pagination: {
-            el: ".swiper-pagination",
-            // 클릭이 가능하도록 처리
-            clickable: true,
-        },
-
-    });
-    
     //  상단영역 기능
     let header = $('.header');
     $(window).scroll(function () {
@@ -69,9 +52,9 @@ $(document).ready(function () {
             header.removeClass('header-active');
         }
 
-        if(scrollbar > 540) {
+        if (scrollbar > 540) {
             gotop.addClass('gotop-active');
-        }else{
+        } else {
             gotop.removeClass('gotop-active');
         }
     });
